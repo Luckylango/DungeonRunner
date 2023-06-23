4875619afe113214eb0d84118159fcf194fba4fb
Luckylango <131213815+Luckylango@users.noreply.github.com>
2023-06-23 13:28:54 +1000

    Big changes
    - Added health and death system
    - Added obstacle course and disabled jumping (Due to bugs)
    - Nerfed dash distance from 3 meters > 1.7 meters

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,26 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public Text healthText;
+    public Image healthBar;
 
-    public int health;
-    public int maxHealth = 10;
+    float health, maxHealth = 100;
+    float lerpSpeed;
 
-    void Start()
+    private void Start()
     {
         health = maxHealth;
     }
-
-    public void TakeDamage(int amount)  
+    private void Update()
     {
-        
+
+        if (health > maxHealth)
+            health = maxHealth;
+
+        lerpSpeed = 3f * Time.deltaTime;
+
+        HealthBarFiller();
+        ColorChanger();
+    }
+
+    public void DecreaseHealth(float amount)
+    {
         health -= amount;
-        
         if (health <= 0)
         {
+
             Destroy(gameObject);
         }
     }
+
+    void HealthBarFiller()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, health / maxHealth, lerpSpeed);
+    }
+
+    void ColorChanger()
+    {
+        Color healthColor = Color.Lerp(Color.red, Color.green, (health / maxHealth));
+
+        healthBar.color = healthColor;
+    }
+
+
+    public void Damage(float damagePoints)
+    {
+        if (health > 0)
+            health -= damagePoints;
+    }
+
+ 
 }
